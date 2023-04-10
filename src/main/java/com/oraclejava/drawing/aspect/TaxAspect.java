0042e22dac8c5dc8a84864d6d0e4a29e3f566352
A <A@DESKTOP-NKSGQVR>
2023-04-10 18:20:03 +0900

    세금포함 금액 1000원입니다.출력안됨

--- src/main/java/com/oraclejava/drawing/aspect/TaxAspect.java
+++ src/main/java/com/oraclejava/drawing/aspect/TaxAspect.java
@@ -1,5 +1,7 @@
 package com.oraclejava.drawing.aspect;
 
+import java.beans.BeanProperty;
+
 import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -7,9 +9,9 @@ import org.aspectj.lang.annotation.Before;
 @Aspect
 public class TaxAspect {
 	
-	@AfterReturning(pointcut = "execution(public String getName())", returning = "name")
-	public void loggingAdvice(String name) {
-		System.out.println("세금포함 금액" + name + " 원입니다.");
+	@AfterReturning(pointcut = "execution(public String getName())", returning = "tax")
+	public void loggingAdvice(String tax) {
+		System.out.println("세금포함 금액" + tax + " 원입니다.");
 	}
 	
 	
